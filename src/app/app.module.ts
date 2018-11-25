64f75373cacdd0a809110dabd2d5b4c29be51890
SKHAIRI MOHAMED AIMANE <skhairimedaimane@gmail.com>
2018-11-26 00:35:26 +0100

    feat: setup projects component into the home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './components/app.component';
 import { SideProfileComponent } from './components/side-profile/side-profile.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideProfileComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
