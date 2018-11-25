99612efbd3d1210ef78c9a2f8d7b0831d830c003
SKHAIRI MOHAMED AIMANE <skhairimedaimane@gmail.com>
2018-11-25 22:58:11 +0100

    feat: pass profile data from app to childrens components

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng7medaimane';
+
+  data: any = {
+    title: 'Software engineer &nbsp;&bull;&nbsp; Mobile Web dev &nbsp;&bull;&nbsp; JavaScript & Python enthusiast.',
+    subtitle: '',
+    description: 'I have always been passionate about computer sciences and discovering new technologies to build innovative solutions.',
+    personalInfos: {
+      fullname: 'Mohamed Aimane Skhairi',
+      email: 'skhairimedaimane@gmail.com',
+      address: 'Tetouan Morocco',
+      phone: '+212678240102',
+      avatar: '../../../assets/images/avatar.jpg'
+    },
+    links: [{
+      name: 'GitHub',
+      icon: 'fa-github',
+      url: 'https://github.com/medaimane',
+      active: true
+    }, {
+      name: 'LinkedIn',
+      icon: 'fa-linkedin',
+      url: 'https://www.linkedin.com/in/skhairimedaimane',
+      active: true
+    }, {
+      name: 'Twitter',
+      icon: 'fa-twitter',
+      url: 'https://twitter.com/med_aimane',
+      active: true
+    }, {
+      name: 'Email',
+      icon: 'fa-envelope-o',
+      url: 'mailto:skhairimedaimane@gmail.com',
+      active: true
+    }]
+  };
 }
--- src/app/component/side-profile/side-profile.component.ts
+++ src/app/component/side-profile/side-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Profile } from 'src/app/interfaces/profile';
 
 @Component({
@@ -8,42 +8,9 @@ import { Profile } from 'src/app/interfaces/profile';
 })
 export class SideProfileComponent implements OnInit {
 
-  profile: Profile;
+  @Input() profile: Profile;
 
   constructor() {
-    this.profile = {
-      title: 'Software engineer &nbsp;&bull;&nbsp; Mobile Web dev &nbsp;&bull;&nbsp; JavaScript & Python enthusiast.',
-      subtitle: '',
-      description: 'I have always been passionate about computer sciences and discovering new technologies to build innovative solutions.',
-      personalInfos: {
-        fullname: 'Mohamed Aimane Skhairi',
-        email: 'skhairimedaimane@gmail.com',
-        address: 'Tetouan Morocco',
-        phone: '+212678240102',
-        avatar: '../../../assets/images/avatar.jpg'
-      },
-      links: [{
-        name: 'GitHub',
-        icon: 'fa-github',
-        url: 'https://github.com/medaimane',
-        active: true
-      }, {
-        name: 'LinkedIn',
-        icon: 'fa-linkedin',
-        url: 'https://www.linkedin.com/in/skhairimedaimane',
-        active: true
-      }, {
-        name: 'Twitter',
-        icon: 'fa-twitter',
-        url: 'https://twitter.com/med_aimane',
-        active: true
-      }, {
-        name: 'Email',
-        icon: 'fa-envelope-o',
-        url: 'mailto:skhairimedaimane@gmail.com',
-        active: true
-      }]
-    };
   }
 
   ngOnInit() {
