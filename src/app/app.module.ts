e618b52c5ad6726b64d9681f25399658cd59452e
SKHAIRI MOHAMED AIMANE <skhairimedaimane@gmail.com>
2018-11-25 23:50:45 +0100

    feat: setup home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './modules/app-routing.module';
-import { AppComponent } from './component/app.component';
-import { SideProfileComponent } from './component/side-profile/side-profile.component';
+import { AppComponent } from './components/app.component';
+import { SideProfileComponent } from './components/side-profile/side-profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideProfileComponent
+    SideProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
