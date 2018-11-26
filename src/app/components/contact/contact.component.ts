379e206eae6c61f7a43ddad317f1be69c05f5ada
SKHAIRI MOHAMED AIMANE <skhairimedaimane@gmail.com>
2018-11-26 22:39:26 +0100

    feat: Add profile data into contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProfileDataService } from 'src/app/services/profile-data.service';
+import { Profile } from 'src/app/interfaces/profile';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  profile: Profile;
+
+  emailhref: string;
+  description: string;
+
+  constructor(private profileData: ProfileDataService) { }
 
   ngOnInit() {
+    this.profile = this.profileData.data;
+    this.emailhref = `mailto:${this.profile.personalInfos.email}`;
+    this.description = 'Let’s build your future solution, tell us about your dream today and let’s do something amazing.';
   }
 
 }
